cee8927e6b9aabb39e4593f89635f3eb2bdcb6f7
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-04-23 12:25:42 -0300

    Listando as Transações

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -83,18 +83,14 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>{formatValue(transaction.value)}</td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
